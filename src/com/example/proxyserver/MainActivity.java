ec7ddeec11321653bf3ea137e67492c4d682ba15
Irena Prochkova <iprockova@gmail.com>
2013-02-19 16:45:19 +0200

    changes to the proxy and ads code

--- src/com/example/proxyserver/MainActivity.java
+++ src/com/example/proxyserver/MainActivity.java
@@ -5,52 +5,84 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 import android.os.Bundle;
+import android.os.Looper;
 import android.app.Activity;
 import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+
+import com.google.ads.*;
+
 public class MainActivity extends Activity {
 
 	 ServerSocket serverSocket = null;
-	  Socket socket = null;
-	  DataInputStream dataInputStream = null;
+	 Socket socket = null;
+	 DataInputStream dataInputStream = null;
+	 
+	private AdView adView;
+	private AdRequest adRequest;
+	LinearLayout layout;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		 Log.w("myApp", "no network");
-		 
-		 new Thread(new Runnable() {
-		        public void run() {
-		            server();
-		        }
-		    }).start();
-		 
-			    
-			
-		 
 	}
-	public void server(){
+	public void startAds(View view){
+		Log.d("myApp", "start ads");
+		adView = new AdView(this, AdSize.BANNER, "a15122003cf3080");
+		
+		layout = (LinearLayout)findViewById(R.id.linearLayout);
+        layout.addView(adView);
+        
+      	new Thread(new Runnable() {
+	        public void run() {
+	        	Looper.prepare();
+	        	adRequest = new AdRequest();
+	            adRequest.addTestDevice(AdRequest.TEST_EMULATOR);              
+	            adRequest.addTestDevice("8C9CFB4E6D4629F186568482BC555C1C"); 
+	        	adView.loadAd(adRequest);
+	        }
+	    }).start();
+		
+		Log.d("myApp", "ads started");
+	}
+	public void startProxy(View view) {
+		Log.d("myApp", "start proxy");
+		
+		new Thread(new Runnable() {
+	        public void run() {
+	            listen();
+	        }
+	    }).start();
+	}
+	public void listen(){
 		try {
 			   serverSocket = new ServerSocket(8888);
-			   System.out.println("Listening :8888");
-			   Log.w("myApp", "no network1");
+			   
+			   Log.d("myApp", "Listening :8888");
+			   
 			  } catch (IOException e) {
 			   // TODO Auto-generated catch block
 			   e.printStackTrace();
 			  }
 		  while(true){
+			  Log.d("myApp", "while");
+			  
 			   try {
-				   System.out.println("Before socket accepted");
-			    socket = serverSocket.accept();
-			    System.out.println("Socket accepted");
+				socket = serverSocket.accept();
+				Log.d("myApp", "socket accepted");
 			    dataInputStream = new DataInputStream(socket.getInputStream());
 			   // dataOutputStream = new DataOutputStream(socket.getOutputStream());
 			    System.out.println("ip: " + socket.getInetAddress());
@@ -88,5 +120,13 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
+	
+	 @Override
+	    public void onDestroy() {
+	      if (adView != null) {
+	        adView.destroy();
+	      }
+	      super.onDestroy();
+	    }
 
 }
