0c6f2f5c9411497212ac2dc3f4ff6cd92d48b2cb
Irena Prochkova <iprockova@gmail.com>
2013-03-06 13:36:34 +0200

    Adding class Cache

---
+++ src/com/example/proxyserver/Cache.java
@@ -0,0 +1,46 @@
+package com.example.proxyserver;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+import com.example.proxyserver.MyHttpResponse;
+
+public class Cache {
+	private LinkedList<HashMap<String, MyHttpResponse>> list;
+	
+	public Cache (){
+		list = new LinkedList<HashMap<String, MyHttpResponse>>();
+	}
+	
+	//insert data into cache
+	public void insert(int location, HashMap<String, MyHttpResponse> simpleList){
+		  list.add(location, simpleList);
+	}
+	public void insertFirst(HashMap<String, MyHttpResponse> simpleList){
+		  list.addFirst(simpleList);
+	}
+	public void insertLast(HashMap<String, MyHttpResponse> simpleList){
+		  list.addLast(simpleList);
+	}
+	
+	//retreive data from cache
+	public HashMap<String, MyHttpResponse> retreive(int location){
+		System.out.println("Retreive data: " + list.get(location));
+		return list.get(location);
+	}
+	
+	//remove data from cache
+	public void remove(int location){
+		list.remove(location);
+	}
+	
+	//print the whole cache
+	public void print(){
+		  Iterator<HashMap<String, MyHttpResponse>> iterator = list.iterator(); 
+		  while (iterator.hasNext()){
+			  System.out.print(iterator.next()+" ");  
+		  }
+		  System.out.println();
+	}
+}
