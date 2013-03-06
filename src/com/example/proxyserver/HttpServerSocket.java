9649b4031dc9d7dfe71b7897a20f33c32390fea8
Irena Prochkova <iprockova@gmail.com>
2013-03-06 13:39:00 +0200

    adding asynctask

--- src/com/example/proxyserver/HttpServerSocket.java
+++ src/com/example/proxyserver/HttpServerSocket.java
@@ -7,29 +7,49 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.util.Log;
 
-public class HttpServerSocket {
+public class HttpServerSocket extends AsyncTask<Void, Void, Void>{
 	private ServerSocket serverSocket = null;
 	private Socket socket = null;
 	
 	private DataInputStream dataInputStream = null;
 	private BufferedReader in = null;
 	
-	public void listen(){
+	private volatile boolean running = true;
+	
+	private static String ADMOB_SERVER_1 = "media.admob.com";
+	private static String ADMOB_SERVER_2 = "googleads.g.doubleclick.net";
+	private static String ADMOB_SERVER_3 = "pagead2.googlesyndication.com";
+	
+	@Override
+	protected void onCancelled() {
+	        running = false;
+	}
+
+
+	@Override
+	protected Void doInBackground(Void... params) {
 		try {
 			   serverSocket = new ServerSocket(8888);
-			   Log.d("myApp", "Listening :8080");
+			   Log.d("myApp", "Listening :8888");
 			   String inputLine;
 
-		  while(true){
+		  while(running){
 			  socket = serverSocket.accept();
 			  Log.d("myApp", "socket accepted");
 			  //dataInputStream = new DataInputStream(socket.getInputStream());
 			  in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-			  while (!(inputLine = in.readLine()).equals(""))
+			  while (!(inputLine = in.readLine()).equals("")) {
 			      System.out.println(inputLine);
+			      if(inputLine.contains(ADMOB_SERVER_1) || inputLine.contains(ADMOB_SERVER_2) || inputLine.contains(ADMOB_SERVER_3)){
+			    	  //contact cache
+			      }
+			  }
 			   }
 		}catch (IOException e) {
 			e.printStackTrace();
@@ -46,5 +66,6 @@ public class HttpServerSocket {
 			      e.printStackTrace();
 			     }
 		}
+		return null;
 	}
 }
